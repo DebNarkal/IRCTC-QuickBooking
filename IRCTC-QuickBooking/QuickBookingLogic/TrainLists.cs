0f86a93fc9a0cbf38a2d5f8afc46c122c4208976
DebNarkal <debnarkal@gmail.com>
2016-10-27 13:42:22 +0530

    Contains the logic of IRCTC-QuickBooking

---
+++ IRCTC-QuickBooking/QuickBookingLogic/TrainLists.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace QuickBookingLogic
+{
+    public static class TrainLists
+    {
+        public static string[] GetTrains(HttpWebRequest req, string postData)
+        {
+            string result;
+            byte[] credentials = Encoding.UTF8.GetBytes(postData);
+            req.ContentLength = credentials.Length;
+            using (Stream stWrite = req.GetRequestStream())
+            {
+                stWrite.Write(credentials, 0, credentials.Length);
+            }
+            HttpWebResponse resp = (HttpWebResponse)req.GetResponse();
+            using (Stream read = resp.GetResponseStream())
+            {
+                using (StreamReader reading = new StreamReader(read))
+                {
+                    result = reading.ReadToEnd();
+                }
+            }
+            JObject jsondata = (JObject)JsonConvert.DeserializeObject(result);
+            string trainInfo = jsondata["data"].ToString();
+            string[] trains = trainInfo.Split(';');
+            return trains;
+        }
+    }
+}
