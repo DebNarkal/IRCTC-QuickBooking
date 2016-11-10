0eb31517187b5540b2b77e13bfc14fcbd34e80c1
DebNarkal <debnarkal@gmail.com>
2016-11-10 12:05:48 +0530

    Added Wait time to overcome the error of AccessViolation exception

--- IRCTC-QuickBooking/IRCTC-QuickBooking/Form1.Designer.cs
+++ IRCTC-QuickBooking/IRCTC-QuickBooking/Form1.Designer.cs
@@ -35,6 +35,8 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
+            this.avlTrains = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
@@ -106,11 +108,34 @@
             this.label3.TabIndex = 7;
             this.label3.Text = "Journey Date:";
             // 
+            // avlTrains
+            // 
+            this.avlTrains.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.avlTrains.Enabled = false;
+            this.avlTrains.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.avlTrains.FormattingEnabled = true;
+            this.avlTrains.Location = new System.Drawing.Point(208, 204);
+            this.avlTrains.Name = "avlTrains";
+            this.avlTrains.Size = new System.Drawing.Size(293, 30);
+            this.avlTrains.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(51, 206);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(148, 22);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Trains Available:";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(924, 451);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.avlTrains);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -133,6 +158,8 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox avlTrains;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- IRCTC-QuickBooking/IRCTC-QuickBooking/Form1.cs
+++ IRCTC-QuickBooking/IRCTC-QuickBooking/Form1.cs
@@ -3,18 +3,23 @@ using System.Collections.Generic;
 using System.Net;
 using System.Windows.Forms;
 using QuickBookingLogic;
+using System.Threading.Tasks;
+using System.Threading;
 
 namespace IRCTC_QuickBooking
 {
     public partial class Form1 : Form
     {
+        TimeSpan waitTimeSpan = new TimeSpan();
         public Form1()
         {
-            InitializeComponent();
             GetStationCodes.PopulateStation();
-            //comboBox1.DataSource = StationCodes.Stations;
+            InitializeComponent();
             doj.MinDate = DateTime.Today;
             doj.MaxDate = doj.MinDate.AddDays(90);
+            doj.Value = DateTime.Today;
+            avlTrains.Text = "Select Train";
+            waitTimeSpan = TimeSpan.FromMilliseconds(50);
         }
 
 
@@ -31,15 +36,22 @@ namespace IRCTC_QuickBooking
 
             string postData = string.Format("stn1={0}&stn2={1}", frmStn.Text.Substring(frmStn.Text.IndexOf('-') + 1), toStn.Text.Substring(toStn.Text.IndexOf('-') + 1));
             string[] resultSet = TrainLists.GetTrains(req, postData);
-
+            List<string> listOfTrains = TrainsForTheDay.trainLists(resultSet, doj.Value.DayOfWeek.ToString());
+            avlTrains.Enabled = true;
+            foreach (var train in listOfTrains)
+            {
+                string[] traindet = train.Split(',');
+                avlTrains.Items.Add(traindet[0] + "-" + traindet[1] + "-" + traindet[2]);
+            }
 
         }
-
         private void frmStn_TextChanged(object sender, EventArgs e)
         {
             AutoCompleteStringCollection allowedTypes = new AutoCompleteStringCollection();
             List<string> res = new List<string>();
             res.Clear();
+            allowedTypes.Clear();
+            Thread.Sleep(waitTimeSpan);
             foreach (string stn in StationCodes.Stations)
             {
                 if (stn.Contains(frmStn.Text.ToUpper()))
@@ -55,6 +67,10 @@ namespace IRCTC_QuickBooking
         {
             AutoCompleteStringCollection allowedTypes = new AutoCompleteStringCollection();
             List<string> res = new List<string>();
+            res.Clear();
+            allowedTypes.Clear();
+            Thread.Sleep(waitTimeSpan);
+
             foreach (string stn in StationCodes.Stations)
             {
                 if (stn.Contains(toStn.Text.ToUpper()))
